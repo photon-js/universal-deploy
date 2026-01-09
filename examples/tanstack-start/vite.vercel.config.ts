e837773725b8b9fc1076ca86c14494d8b0e88804
Joël Charles <joel.charles91@gmail.com>
2026-01-09 14:48:47 +0100

    test(vercel): building tanstack app with vite-plugin-vercel

---
+++ examples/tanstack-start/vite.vercel.config.ts
@@ -0,0 +1,17 @@
+import { vercel } from "vite-plugin-vercel/vite";
+import config from "./vite.common.config";
+
+config.plugins ??= [];
+// Enables building for Vercel (development and deploy builds).
+// vite-plugin-vercel@11 natively supports @universal-deploy/store
+config.plugins.push(
+  vercel({
+    viteEnvNames: {
+      client: "client",
+      node: "ssr",
+      edge: false,
+    },
+  }),
+);
+
+export default config;
