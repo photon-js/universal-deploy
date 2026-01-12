b92b29ed8f8998ad11513726d2dfac7b80c8476a
Romuald Brillout <git@brillout.com>
2026-01-12 16:05:18 +0100

    wip

--- examples/app-vercel/vite.config.ts
+++ examples/app-vercel/vite.config.ts
@@ -4,7 +4,7 @@ import { vercel } from "vite-plugin-vercel/vite";
 
 export default defineConfig({
   plugins: [
-    // TODO: this sentence isn't clear (I managed to understand with brain power because we discussed it)
+    // TODO: this sentence isn't clear (I managed to understand it only with brain power and only because we discussed it)
     // Version of vite-plugin-vercel that reads entries from @universal-deploy/store
     vercel(),
     // Minimal SSR framework. Includes devServer and catchAll plugins from @universal-deploy/store
