e0d39f50359e576564cb58fbff8b2b529f96fa2c
Joël Charles <joel.charles91@gmail.com>
2026-01-20 15:21:47 +0100

    feat: some more plugins

---
+++ packages/store/src/vite/hmr.ts
@@ -0,0 +1,25 @@
+import type { Plugin } from "vite";
+import { isServerEntry } from "../utils.js";
+
+export function hmr(): Plugin {
+  return {
+    name: "photon:hmr",
+    apply: "serve",
+
+    transform(code: string, id: string) {
+      if (!isServerEntry(this, id)) return;
+
+      const transformed = `${code}
+
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
+`;
+
+      return {
+        code: transformed,
+        map: null,
+      };
+    },
+  };
+}
