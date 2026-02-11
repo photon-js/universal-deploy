41d94593abffa78232dd7d0a3134d42dab0f3168
Joël Charles <joel.charles91@gmail.com>
2026-02-11 13:24:02 +0100

    test

--- examples/tanstack-start/.test-dev.test.ts
+++ examples/tanstack-start/.test-dev.test.ts
@@ -1,3 +1,9 @@
 import { testRun } from "./testRun.js";
 
-testRun("pnpm run dev --strictPort --port 3000");
+testRun("pnpm run dev --strictPort --port 3000", {
+  // pnpm run dev tends to exit with code 143 for some reason
+  tolerateExitCode: [143],
+  tolerateError({ logText, logSource }) {
+    if (logSource === "run() failure" && logText.includes("error code 143")) return true;
+  },
+});
